c17fa58cd1fee64aac26af7d7a8d2c3e1ead3392
JackLikesHotpot <47116970+JackLikesHotpot@users.noreply.github.com>
2024-02-13 17:37:36 +0000

    added your_url.html as a form action url to display form, will adjust later. modified app.py to handle requests.

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return render_template('home.html', name='jack')
+    return render_template('home.html')
 
 
-@app.route('/about')
-def about():
-    return 'This is a Reddit s!'
-
+@app.route('/your-url', methods=['GET', 'POST'])
+def your_url():
+    if request.method == 'POST':
+        return render_template('your_url.html', code=request.form['code'])
+    else:
+        return 'invalid'
 # pipenv shell
 # flask run --debug
