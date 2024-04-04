d0fbbaa8a97ff1eda7878580aabe8bcde282f558
JackLikesHotpot <47116970+JackLikesHotpot@users.noreply.github.com>
2024-04-04 02:17:54 +0100

    enabled nsfw checks, removed limits, and made it so only images can display

--- process.py
+++ process.py
@@ -1,12 +1,15 @@
 import praw
+import sys
 
 def search_saved(reddit, preferences):
     saved_output = {}
-    for item in reddit.user.me().saved(limit=int(preferences['limit'])):
-        if isinstance(item, praw.models.Submission):
-            if 'nsfw' not in preferences:  # if you don't pick nsfw
-                # not working i think
-                if not item.over_18:
+    print(sum(1 for _ in reddit.user.me().saved(limit=25)), file=sys.stderr)
+    for item in reddit.user.me().saved():
+        if isinstance(item, praw.models.Submission) and not item.is_self:
+            if 'nsfw' not in preferences and item.over_18:  # if you don't pick nsfw
+                continue
+            else:
+                if item.url.endswith('png') or item.url.endswith('jpeg'):
                     saved_output[item.title] = [item.url, item.subreddit.display_name]
     return saved_output
 
