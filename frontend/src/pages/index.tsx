bd21e59c719d1708c232c683879038a0c7186044
JackLikesHotpot <47116970+JackLikesHotpot@users.noreply.github.com>
2025-01-31 04:32:46 +0000

    added styles to front page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,6 +2,13 @@ import React, { useState, useEffect } from 'react';
 import styles from '../styles/Index.module.css'; // Assuming the CSS is in the 'src/styles' directory
 import axios from 'axios'
 import Link from 'next/link';
+import { Noto_Sans } from 'next/font/google';
+
+const notoSans = Noto_Sans({
+  subsets: ['latin'],
+  weight: ['100', '400', '700'],
+  display: 'swap',
+});
 
 export default function Home() {
   const [authUrl, setAuthUrl] = useState('https://www.reddit.com');
@@ -24,17 +31,23 @@ export default function Home() {
   return (
       <div className={styles['landing']}>
         <div className={styles['panel']}></div>
-          <div className={styles['description']}>
-            <ul className={styles['subheading']}>
-              <li>Lets you view up to 1000 of your saved Reddit images!</li>
-              <li>You can filter your own images!</li>
-              <li>Doesn't save your own data!</li>
-            </ul>
+        <div className={styles['body']}>
+          <div className={`${styles['heading']} ${notoSans.className}`}>
+            <span>Saved by Snoo</span>
           </div>
-          <div className={styles['login']}>
-            <Link href={authUrl} className={styles['button']}>
-              Login with Reddit
-            </Link></div>
+            <div className={styles['description']}>
+              <ul className={styles['subheading']}>
+                <li>View up to 1000 of your saved Reddit images!</li>
+                <li>You can filter by subreddit!</li>
+                <li>Doesn't save your own credentials!</li>
+              </ul>
+            <div className={styles['login']}>
+              <Link href={authUrl} className={styles['button']}>
+                Login with Reddit
+              </Link>
+            </div>
+          </div>
+        </div>
         <div className={styles['panel']}></div>
       </div>
   );
