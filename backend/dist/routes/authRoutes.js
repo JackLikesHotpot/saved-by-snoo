4705db2c3a591389cbd52ec31a4832718550389a
JackLikesHotpot <47116970+JackLikesHotpot@users.noreply.github.com>
2025-04-09 16:16:04 +0100

    removed reference to dist folder as no longer needed, deleted flask files

--- backend/dist/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const authController_1 = require("../controllers/authController");
-const router = (0, express_1.Router)();
-router.get('/api/auth_url', authController_1.getAuthUrl);
-router.get('/auth_callback', authController_1.authCallback);
-exports.default = router;
